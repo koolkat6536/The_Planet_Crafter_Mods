816dea48790088d4d05e2cc93b7617f89f665d87
Askar0 <46961993+Askar0@users.noreply.github.com>
2022-05-08 13:42:36 +0800

    Delete EnhancedMinimap/obj directory

--- EnhancedMinimap/obj/Release/net46/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace EnhancedMinimap
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "EnhancedMinimap";
-        public const string PLUGIN_NAME = "EnhancedMinimap";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
